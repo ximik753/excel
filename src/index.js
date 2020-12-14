b792af64329c1f0e6628869b2acac715fcde8a93
Ximik <34097384+ximik753@users.noreply.github.com>
2020-12-14 11:47:55 +0300

    Framework start (#3)
    * finish base framework functional
    
    * comment debug

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import './scss/index.scss'
+import { Excel } from '@/components/excel/Excel'
+import { Header } from '@/components/header/Header'
+import { Toolbar } from '@/components/toolbar/Toolbar'
+import { Table } from '@/components/tabel/Table'
+import { Formula } from '@/components/formula/Formula'
 
-console.log('Work')
+const excel = new Excel('#app', {
+    components: [Header, Toolbar, Formula, Table]
+})
+
+excel.render()
