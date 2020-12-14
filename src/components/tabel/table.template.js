8247a6fefd7b46d5dfffc83c314e32a884cbe5fe
ximik_753 <sankovdiman@mail.ru>
2020-12-14 11:39:54 +0300

    finish base framework functional

---
+++ src/components/tabel/table.template.js
@@ -0,0 +1,49 @@
+const CODES = {
+    A: 65,
+    Z: 90
+}
+
+function createCol (content) {
+    return `<div class="column">${content}</div>`
+}
+
+function createCell (content) {
+    return ` <div class="cell">${content}</div>`
+}
+
+function createRow (index, content) {
+    return `
+        <div class="row">
+            <div class="row-info">${index || ''}</div>
+            <div class="row-data">${content}</div>
+        </div>
+    `
+}
+
+function toChar (_, index) {
+    return String.fromCharCode(CODES.A + index)
+}
+
+export function createTable (rowsCount = 25) {
+    const colsCount = CODES.Z - CODES.A + 1
+    const rows = []
+
+    const cols = new Array(colsCount)
+        .fill('')
+        .map(toChar)
+        .map(createCol)
+        .join('')
+
+    rows.push(createRow(null, cols))
+
+    for (let i = 0; i < rowsCount; i++) {
+        const cells = new Array(colsCount)
+            .fill('')
+            .map(createCell)
+            .join('')
+
+        rows.push(createRow(i + 1, cells))
+    }
+
+    return rows.join('')
+}
